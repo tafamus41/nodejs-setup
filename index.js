cb3ab90190d4ffc3d7c20c79c87bfcdbe7f0104f
asimov <seyranmustafa4@gmail.com>
2024-11-13 21:16:35 +0300

    p

--- index.js
+++ index.js
@@ -6,3 +6,6 @@
 //çoklu ve tire tireli yorumun kısayolu nasıldı//
 //js de direk object oluşturabilirim (class'a ihtiyaç duymadan)
 // obje isimlendirmede PascalCase veya camelCase yapabiliriz
+/* -------------------------------------------------------------------------- */
+/*                                    naber                                   */
+/* -------------------------------------------------------------------------- */
