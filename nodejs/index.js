286bc7fb0be4e12a7e578d5f3dd34b15ed6291f1
asimov <seyranmustafa4@gmail.com>
2024-11-16 12:24:37 +0300

    p

---
+++ nodejs/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//const obj = require("./modules");
+//const obj = require("./modules/index.js");
+const obj = require("./modules/index");
+console.log(obj);
+
+// console.log(obj.x);
+// obj.y;
